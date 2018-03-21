2066fce039f3e30fc2a29e157be9b2250ed3f623
Han Jung <hanuljung10@gmail.com>
2018-03-20 23:30:48 -0700

    put date
    put date

--- venv/Storage/dynomodb.py
+++ venv/Storage/dynomodb.py
@@ -1,4 +1,5 @@
 import boto3
+import dateutil.parser
 from venv.Storage.keyvalue_store import KeyValueStore
 
 
@@ -9,9 +10,14 @@ class DynomoDB(KeyValueStore):
 
     def set(self, id_of : str, properties : dict):
         # table.put_item(Item={'name':'tom', 'age':40, 'height':6, 'fans':10000})
-        self.__table.put_item(Item=properties)
+        self.__table.put_item(Item={
+                                    'id':str(properties['id']),
+                                    'name':str(properties['name']),
+                                    'parent':str(properties['parent']),
+                                    'created_time':properties['created_time'].isoformat()})
         pass
 
     def get(self, pattern : str):
         items = self.__table.scan()
+        #yourdate = dateutil.parser.parse(datestring)
         return items['Items']
